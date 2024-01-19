e180ca224447e8fad93acf956314a4704c53022e
Dandanpan258 <157072009+Dandanpan258@users.noreply.github.com>
2024-01-19 13:19:33 +1100

    Add files via upload

--- helloword.py
+++ helloword.py
@@ -1,1 +1,23 @@
+#website:https://www.w3schools.com/python/python_variables_output.asp
 print("welcome to the OOP week1!")
+
+#Variable Names
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+
+#Multiple Variables
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+
+#Output Variables
+x = "Python"
+y = "is"
+z = "awesome"
+print(x, y, z)
+
